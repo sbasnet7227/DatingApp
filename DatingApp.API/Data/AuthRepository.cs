c48679e6874fce09f696bbc947137b4f22b75b63
Sunil Git <basnet_sunil@hotmail.com>
2019-05-26 17:57:29 -0700

    End of Section 3

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
@@ -12,15 +13,46 @@ namespace DatingApp.API.Data
         {
             _context = context;
         }
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            if(user == null)
+            {
+                return null;
+            }
+
+            if(!VerifyPaswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            }
+            return user;
+
+        }
+
+        private bool VerifyPaswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+               
+               var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+               for(int i = 0; i<computedHash.Length; i++)
+               {
+                   if(computedHash[i] != passwordHash[i])
+                   {
+                       return false;
+
+                   }
+               }
+
+            }
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
         {
             byte[] passwordHash, passwordSalt;
-            createPasswordHash(password, out passwordHash, out passwordSalt);
+            createPasswordHash(password, out passwordHash, out passwordSalt); // out makes reference type and value updated autoatically
 
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
@@ -41,9 +73,15 @@ namespace DatingApp.API.Data
             }
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if(await _context.Users.AnyAsync(x => x.Username == username))
+            {
+                return true;
+            }
+
+            return false;
+            
         }
     }
 }
