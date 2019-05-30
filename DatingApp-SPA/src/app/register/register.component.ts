cfaf093d7ca477e390fa3544d64d12ba2f14fc92
Sunil Git <basnet_sunil@hotmail.com>
2019-05-30 00:50:54 -0700

    initial commit

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
-import { NgForm, NgModel } from '@angular/forms';
+import { AlertifyService } from '../_services/alertify.service';
+
 
 
 @Component({
@@ -12,22 +13,21 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('registration successful');
+     this.alertify.success('registration successful');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancelled');
   }
 
 }
