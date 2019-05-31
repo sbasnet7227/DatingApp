4ce1eeec0b9dc187e00f811258d47e3ebf5cde00
Sunil Git <basnet_sunil@hotmail.com>
2019-05-30 20:49:26 -0700

    End of Section 6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import { AppComponent } from './app.component';
 import { HttpClient } from 'selenium-webdriver/http';
-import { NavComponent } from './nav/nav.component';
 import { FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
@@ -21,7 +23,8 @@ import { AlertifyService } from './_services/alertify.service';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
